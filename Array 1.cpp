7899495c950c685eba40bcb62633c873730ece90
Brijesh kumar <brijeshpal8878@gmail.com>
2024-03-16 15:32:41 +0530

    Add files via upload

---
+++ Array 1.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int arr[6];
+	cout<<"Enter the array: ";
+	cin>>arr[0]>>arr[1]>>arr[2]>>arr[3]>>arr[4]>>arr[5];
+	cout<<arr[2];
+	return 0;
+}
